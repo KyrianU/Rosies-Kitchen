e2ce1ad0c6ec75084001c86704ee391642c86ff7
KyrianU <kyrian.uwaegbulem@gmail.com>
2022-11-08 00:37:27 +0000

    structed to forms.py

---
+++ reservation/forms.py
@@ -0,0 +1,17 @@
+from django import forms 
+from .models import Reservation, Customer
+from django.conf import settings
+
+
+class CustomerForms(forms.ModelForm):
+
+    class Meta:
+        model = Customer
+        fields = ('full_name', 'email')
+
+
+class ReservationForm(forms.ModelForm):
+  
+    class Meta:
+        model = Reservation
+        fields = ('no_of_guest', 'requested_date', 'requested_time')
