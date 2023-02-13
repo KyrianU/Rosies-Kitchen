4aa5e64e5c778240df928a82cc6e3d5e096e62e1
KyrianU <kyrian.uwaegbulem@gmail.com>
2023-02-13 15:44:33 +0000

    changed STATICFILES_STORAGE settings

--- rosies_kitchen/settings.py
+++ rosies_kitchen/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILE_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR / "static"),)
 
